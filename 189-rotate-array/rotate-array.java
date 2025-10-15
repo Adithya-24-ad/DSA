92cc62cbf2c097c2bd9d583ffb1cc6ca59ab17da
Adithya-24-ad <orewa7adithya@gmail.com>
2025-10-15 12:27:57 +0530

    Time: 2 ms (25.33%) | Memory: 57.4 MB (68.41%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -4,8 +4,7 @@ class Solution {
         k = k % n;
         int [] temp = new int[n];
         for(int i = 0;i < n;i++){
-            int newindex = (i + k )% n;
-            temp[newindex] = nums[i];
+             temp[(i + k) % n] = nums[i];
         }
         for(int i = 0;i < n;i++){
             nums[i] = temp[i];

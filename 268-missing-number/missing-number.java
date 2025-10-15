3ffebb641ea92a64b421925727c7bd1630f1e341
Adithya-24-ad <orewa7adithya@gmail.com>
2025-10-15 11:45:27 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (86.15%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int res = nums.length;
+        int miss = nums.length;
         for(int i = 0;i<nums.length;i++){
-            res += i-nums[i];
+            miss += i-nums[i];
         }
-        return res;
+        return miss;
     }
 }
